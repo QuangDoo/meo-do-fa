4eb9920fcc0212c9bab144eba45b8df598015872
Bùi Trung Bảo <buitrbao222@gmail.com>
2020-10-20 21:17:55 +0700

    [MED-33] Component thông báo error

--- src/components/Register/RegisterForm.tsx
+++ src/components/Register/RegisterForm.tsx
@@ -1,20 +1,22 @@
 import React, { FC } from 'react'
 import { useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
+import Button from '../Button'
+import Checkbox from '../Form/Checkbox'
+import Input from '../Form/Input'
 import AccountInformation from './AccountInformation'
 import ChooseUserType from './ChooseUserType'
 import WelcomeAccount from './WelcomeAccount'
 
 export type UserType = 'pharmacy' | 'clinic' | 'drugstore'
 
-// Map from userType to text (replace with translation later like t('pharmacy') or something)
+// Map from UserType to text
+// (replace with translation later like t('pharmacy') or something)
 export const userTypeMap: Record<UserType, string> = {
   pharmacy: 'Nhà thuốc',
   clinic: 'Phòng khám',
   drugstore: 'Quầy thuốc',
 }
-
-// RegisterForm Props
-type Props = {}
 
 // Form input fields
 type Inputs = {
@@ -29,6 +31,9 @@ type Inputs = {
 // Initial userType state
 const initialUserType = ''
 
+// RegisterForm Props
+type Props = undefined
+
 const RegisterForm: FC<Props> = (props) => {
   const { register, handleSubmit, setValue, watch } = useForm<Inputs>()
 
@@ -39,6 +44,7 @@ const RegisterForm: FC<Props> = (props) => {
   // On submit button click
   const onSubmit = (data: Inputs) => {
     console.log('submit data:', data)
+    toast.error('Clicked submit data')
   }
 
   // Set user type on UserTypeCard click (in ChooseUserType)
@@ -51,9 +57,15 @@ const RegisterForm: FC<Props> = (props) => {
 
   return (
     <form className="new_account" onSubmit={handleSubmit(onSubmit)}>
+      {/**
+       * Hidden input for userType
+       * Value changes when clicking a UserTypeCard
+       */}
+      <input name="userType" hidden type="text" ref={register} />
+
       {/* Hide ChooseUserType if userType is in initial state */}
       <div hidden={watchUserType !== initialUserType} className="business-group">
-        <ChooseUserType setUserType={setUserType} ref={register} />
+        <ChooseUserType setUserType={setUserType} />
       </div>
 
       {/* Hide AccountInformation form if userType is chosen (NOT in initial state) */}
@@ -62,7 +74,76 @@ const RegisterForm: FC<Props> = (props) => {
           userTypeName={userTypeMap[watchUserType]} // Map from userType to text (to use on translation later)
           onEditClick={resetUserType}
         />
-        <AccountInformation ref={register} />
+
+        <Input
+          name="name"
+          ref={register}
+          containerClass="mb-4"
+          iconClass="icomoon icon-user"
+          placeholder="Nhập tên (bắt buộc)"
+          required
+        />
+
+        <Input
+          name="phone"
+          type="number"
+          ref={register}
+          containerClass="mb-4"
+          iconClass="icomoon icon-phone"
+          placeholder="Nhập số điện thoại (bắt buộc)"
+          required
+        />
+
+        <Input
+          name="email"
+          ref={register}
+          containerClass="mb-4"
+          iconClass="icomoon icon-mail"
+          placeholder="Nhập email"
+        />
+
+        <Input
+          name="password"
+          ref={register}
+          containerClass="mb-4"
+          iconClass="icomoon icon-lock"
+          placeholder="Nhập mật khẩu (bắt buộc)"
+          type="password"
+          required
+        />
+
+        <Input
+          name="referPhone"
+          ref={register}
+          containerClass="mb-4"
+          iconClass="fas fa-user-friends"
+          placeholder="Số điện thoại người giới thiệu hoặc mã nhóm"
+          type="number"
+        />
+
+        <Checkbox
+          required
+          label={
+            <>
+              Tôi đã đọc và đồng ý với{' '}
+              <a href="/terms-and-condition" target="_blank">
+                Điều khoản sử dụng
+              </a>
+              <span className="text-danger"> *</span>
+            </>
+          }
+        />
+
+        <div className="mb-4">
+          Nếu bạn đã có tài khoản, vui lòng{' '}
+          <a className="text-secondary" data-modal="true" href="/authentications/login">
+            Đăng nhập
+          </a>
+        </div>
+
+        <Button type="submit" variant="gradient" block>
+          Tạo tài khoản
+        </Button>
       </div>
     </form>
   )
