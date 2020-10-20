1e155cda904f236386cd88431d39cbcde214cbad
Quang <quang.do@bebuy.vn>
2020-10-20 18:00:39 +0700

    [MED-95] Layout Login

--- src/components/Login/AccountLoginInformation.tsx
+++ src/components/Login/AccountLoginInformation.tsx
@@ -7,13 +7,15 @@ const AccountLoginInformation = (props: Props, formRef) => {
     return (
         <>
             <Input name="name"
-                ref={formRef}
+                ref={formRef({ pattern: /^[A-Za-z]+$/i })}
                 containerClass="mb-4"
                 iconClass="icomoon icon-user"
-                placeholder="Nhập số điện thoại hoặc email" />
+                required={true}
+                placeholder="Nhập số điện thoại hoặc email"/>
             <Input name="password"
                 ref={formRef}
                 containerClass="mb-4"
+                required={true}
                 iconClass="icomoon icon-lock"
                 placeholder="Nhập mật khẩu"
                 type="password" />
--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -5,17 +5,19 @@ import Checkbox from '../Form/Checkbox'
 import AccountLoginInformation from './AccountLoginInformation'
 type Props = {}
 type Inputs = {
-    userType: string
+    data: string
 }
 const LoginForm: FC<Props> = (props) => {
-    const { register, handleSubmit, setValue, watch } = useForm<Inputs>({
+    const { register, handleSubmit, setValue, watch, errors } = useForm<Inputs>({
         defaultValues: {
-            userType: '',
+            data: '',
         },
     })
-    console.log('watch', watch)
+    console.log('errors', errors)
+    console.log('watch', watch())
     const submit = (data) => {
         console.log('data', data)
+        
     }
     return (
         <div>
@@ -28,8 +30,9 @@ const LoginForm: FC<Props> = (props) => {
                     <a data-modal="true" href="/authentications/reset_password">Quên mật khẩu</a>
                 </div>
                 <Button variant="gradient" block>Đăng nhập</Button>
-                <span className="text-capitalize">
+                <span className="text-capitalize ">
                     Để nhận ưu đãi hấp dẫn,
+                    
                     <a className="text-secondary ml-1" data-modal="true" href="/authentications/signup">
                         <b>đăng ký thành viên</b>
                     </a>.
