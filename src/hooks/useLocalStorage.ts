fcde8f5aead65174375fb47afa494546257557a3
Trung Bao <buitrbao222@gmail.com>
2020-10-26 10:41:45 +0700

    Fix localStorage bug.

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react'
 
 export default function useLocalStorage<T>(key: string) {
-  const [storedValue, setStoredValue] = useState<T>(JSON.parse(localStorage.getItem(key)))
+  const [storedValue, setStoredValue] = useState<T>()
 
   // Update state when local storage changes
   useEffect(() => {
     function updateData() {
       setStoredValue(JSON.parse(localStorage.getItem(key)))
     }
+
+    updateData()
 
     window.addEventListener('storage', updateData)
 
