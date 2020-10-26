ee5f3afd6b74a2d964a7a709cda70cac43faf6da
Trung Bao <buitrbao222@gmail.com>
2020-10-26 11:11:16 +0700

    Update.

--- src/hooks/useIsLoggedIn.ts
+++ src/hooks/useIsLoggedIn.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import useLocalStorage from './useLocalStorage'
 
 export default function useIsLoggedIn() {
-  const [token] = useLocalStorage('token')
+  const [token, setToken] = useLocalStorage('token')
 
   const [isLoggedIn, setIsLoggedIn] = useState(!!token)
 
