140f528bf910460f11a5c0ae39f4bdc1a826fdf1
Quang <quang.do@bebuy.vn>
2020-10-29 17:10:07 +0700

    [MED-136] layout Hoạt chất detail done

--- src/pages/ingredients/[ingredientId].tsx
+++ src/pages/ingredients/[ingredientId].tsx
@@ -4,8 +4,13 @@ import { Header } from '../../components/Header'
 import Footer from '../../components/Footer'
 import Head from '../../components/Head'
 import { Nav } from '../../components/Nav'
+import Tab from '../../components/Tab/Tab'
+import { ProductsContainer } from '../../components/Home/ProductsContainer'
+import { ProductsCarousel } from '../../components/ProductsCarousel'
+import { exampleProducts } from '../../components/Products'
 
 export default function IngredientDetail(props) {
+  const tabContent = { thongTinChung: 'abc' }
   return (
     <>
       <Head>
@@ -14,7 +19,18 @@ export default function IngredientDetail(props) {
       <Header />
       <Nav />
       <Layout>
-        <div>Component chi tiet hoạt chất ở đây</div>
+        <main className="ingredient container py-3 py-sm-5">
+          <div className="row justify-content-center">
+            <div className="col-sm-8">
+              <h1 className="mb-3">Acetazolamid</h1>
+            </div>
+            <Tab {...tabContent} />
+          </div>
+        </main>
+        <hr />
+        <ProductsContainer title="Danh sách các thuốc có Acetazolamid" seeMoreUrl="#">
+          <ProductsCarousel products={exampleProducts} />
+        </ProductsContainer>
       </Layout>
       <Footer />
     </>
