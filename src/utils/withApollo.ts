ebd0db4e12c54f833a0b0c21cbc9c4ae8584a3c0
Trung Bao <buitrbao222@gmail.com>
2020-11-09 11:23:57 +0700

    Update.

--- src/utils/withApollo.ts
+++ src/utils/withApollo.ts
@@ -1,8 +1,25 @@
-import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { withApollo } from 'next-apollo';
 
+const httpLink = createHttpLink({
+  uri: 'https://graphql.medofa.bedigital.vn'
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
 const apolloClient = new ApolloClient({
-  uri: 'https://graphql.medofa.bedigital.vn',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
