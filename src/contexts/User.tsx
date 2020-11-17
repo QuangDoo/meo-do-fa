0c848be8e19d7c5336f68843ba814dcd461deb4f
Trung Bao <buitrbao222@gmail.com>
2020-11-17 16:59:44 +0700

    Integrate global User context.

--- src/contexts/User.tsx
+++ src/contexts/User.tsx
@@ -1,25 +1,36 @@
-import { useQuery } from '@apollo/react-hooks';
-import React, { createContext, useContext } from 'react';
+import { QueryLazyOptions, useLazyQuery } from '@apollo/react-hooks';
+import React, { createContext, useContext, useEffect } from 'react';
 import { GET_USER, GetUserData } from 'src/graphql/user/getUser.mutation';
+import { User } from 'src/types/User';
 import withApollo from 'src/utils/withApollo';
 
 type Props = {
   children: React.ReactNode;
 };
 
-const UserContext = createContext(null);
+type ContextValue = {
+  user?: User;
+  getUser: (options?: QueryLazyOptions<undefined>) => void;
+};
+
+const UserContext = createContext<ContextValue>(null);
 
 const UserProvider = withApollo({ ssr: true })(({ children }: Props) => {
-  const { data } = useQuery<GetUserData, undefined>(GET_USER, {
-    onCompleted: (data) => {
-      console.log('User data:', data);
-    },
+  const [getUser, { data }] = useLazyQuery<GetUserData, undefined>(GET_USER, {
     onError: (error) => {
       console.log('Get user error:', { error });
     }
   });
 
-  return <UserContext.Provider value={{ user: 'test' }}>{children}</UserContext.Provider>;
+  useEffect(() => {
+    if (!localStorage.getItem('token')) return;
+
+    getUser();
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user: data?.getUser, getUser }}>{children}</UserContext.Provider>
+  );
 });
 
 const useUser = () => useContext(UserContext);
