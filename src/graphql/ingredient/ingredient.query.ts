deb9fe91cd86af66855baf9d004fd85b4bda498c
Bùi Trung Bảo <buitrbao222@gmail.com>
2020-11-19 09:22:44 +0700

    Done manufacturers page

--- src/graphql/ingredient/ingredient.query.ts
+++ src/graphql/ingredient/ingredient.query.ts
@@ -1,14 +1,20 @@
 import { gql } from 'apollo-boost';
 
-export const GET_INGREDIENTS = gql`
-  query getAllGredients($page: Int!, $pageSize: Int!) {
-    getIngredients(input: { setPage: { page: $page, pageSize: $pageSize } }) {
+export const GET_ALL_INGREDIENTS = gql`
+  query {
+    getIngredientsAll {
       id
       name
-      slug
     }
   }
 `;
+
+export type GetAllIngredientsData = {
+  getIngredientsAll: {
+    id: string;
+    name: string;
+  }[];
+};
 
 export const GET_INGREDIENT_DETAILS = gql`
   query($id: Int!) {
