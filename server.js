89a5ea1e001ad9bb230ce3aff76a0bbec84afcbb
tt <thanh-tai.truong@ekino.com>
2020-11-26 12:00:59 +0700

    fix server

--- server.js
+++ server.js
@@ -1,38 +1,40 @@
-const express = require('express');
-const next = require('next');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const express = require('express');
+// const next = require('next');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const port = process.env.PORT || 3000;
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+// const port = process.env.PORT || 3000;
+// const dev = process.env.NODE_ENV !== 'production';
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
 
-const isDevelopment = process.env.NODE_ENV !== 'production';
+// const isDevelopment = process.env.NODE_ENV !== 'production';
 
-app
-  .prepare()
-  .then(() => {
-    const server = express();
+// app
+//   .prepare()
+//   .then(() => {
+//     const server = express();
 
-    server.use(
-      '/graphql',
-      createProxyMiddleware({
-        target: 'http://gateway.medofa.svc.cluster.local',
-        changeOrigin: false,
-        ws: true,
-        auth: 'medofa:medofa-bb-999'
-      })
-    );
+//     const filter = function (pathname, req) {
+//       return pathname.match('^/graphql') && req.method === 'POST';
+//     };
 
-    server.all('*', (req, res) => {
-      return handle(req, res);
-    });
+//     const apiProxy = createProxyMiddleware(filter, {
+//       target: 'http://gateway.medofa.svc.cluster.local',
+//       changeOrigin: false,
+//       ws: true
+//     });
 
-    server.listen(port, (err) => {
-      if (err) throw err;
-      console.log(`> Ready on http://localhost:${port}`);
-    });
-  })
-  .catch((err) => {
-    console.log('Error:::::', err);
-  });
+//     server.use(apiProxy);
+
+//     server.all('*', (req, res) => {
+//       return handle(req, res);
+//     });
+
+//     server.listen(port, (err) => {
+//       if (err) throw err;
+//       console.log(`> Ready on http://localhost:${port}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log('Error:::::', err);
+//   });
