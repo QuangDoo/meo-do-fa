1d7a1fe0e9cc525c38d6e82b34f0d9b2727831b1
tt <thanh-tai.truong@ekino.com>
2020-11-29 13:35:16 +0700

    adding pwa

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
-const { nextI18NextRewrites } = require('next-i18next/rewrites')
+const { nextI18NextRewrites } = require('next-i18next/rewrites');
+const withPWA = require('next-pwa');
+const localeSubpaths = {};
 
-const localeSubpaths = {}
-
-module.exports = {
+module.exports = withPWA({
   rewrites: async () => nextI18NextRewrites(localeSubpaths),
   publicRuntimeConfig: {
-    localeSubpaths,
+    localeSubpaths
   },
   env: {
-    GRAPHQL_GATEWAY: process.env.GRAPHQL_GATEWAY,
+    GRAPHQL_GATEWAY: process.env.GRAPHQL_GATEWAY
   },
-}
+  pwa: {
+    dest: 'public'
+  }
+});
