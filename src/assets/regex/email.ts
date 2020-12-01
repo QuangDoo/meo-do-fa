45d9800b3ed1e6c16626d97d7766088840847cb5
Trung Bao <buitrbao222@gmail.com>
2020-12-01 12:00:55 +0700

    Update register email validation - no special characters

--- src/assets/regex/email.ts
+++ src/assets/regex/email.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-useless-escape */
+export const emailRegex = /^\S+@\S+$/i;
 
-// Email validation RegExp from https://stackoverflow.com/questions/46155
-export const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+export const noSpecialChars = /^[^~!#$%^&*()\-+_=[\]\\{}|;:'",<>/?]*$/;
