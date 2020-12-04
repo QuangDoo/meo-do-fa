42b1a21583f96436f625cf89ce4eab930a865b9a
TrangJuny <thuytrang.mis93@gmail.com>
2020-12-04 11:07:01 +0700

    update code

--- src/components/Modules/Home/ProductsContainer/index.tsx
+++ src/components/Modules/Home/ProductsContainer/index.tsx
@@ -13,22 +13,24 @@ export const ProductsContainer: FC<ProductsContainerProps> = (props) => {
   const dealsClass = props.deals ? 'deals' : '';
   const { t } = useTranslation(['common']);
   return (
-    <section className={`py-5 container ${dealsClass} ${props.className}`}>
-      <div className="text-center mb-4">
-        <h2 className={props.deals && 'text-white'}>{props.title}</h2>
-      </div>
-
-      <div className="mb-4">{props.children}</div>
-
-      {props.seeMoreUrl && (
-        <div className="text-center">
-          <Button
-            variant={props.deals ? 'outline-light' : 'outline-primary'}
-            className={props.deals ? 'btn-transparent' : 'btn-white'}>
-            {t('common:see_all')}
-          </Button>
+    <section className={`py-5 ${dealsClass} ${props.className}`}>
+      <div className="container">
+        <div className="text-center mb-4">
+          <h2 className={props.deals && 'text-white'}>{props.title}</h2>
         </div>
-      )}
+
+        <div className="mb-4">{props.children}</div>
+
+        {props.seeMoreUrl && (
+          <div className="text-center">
+            <Button
+              variant={props.deals ? 'outline-light' : 'outline-primary'}
+              className={props.deals ? 'btn-transparent' : 'btn-white'}>
+              {t('common:see_all')}
+            </Button>
+          </div>
+        )}
+      </div>
     </section>
   );
 };
