6a4390cdef7e019661f12d88b4e34322aa8e18aa
Trung Bao <buitrbao222@gmail.com>
2020-12-11 16:43:13 +0700

    Add loading backdrop for cart and checkout, only create counsel and order once

---
+++ src/components/Layout/LoadingBackdrop.tsx
@@ -0,0 +1,25 @@
+import { Backdrop, CircularProgress, makeStyles } from '@material-ui/core';
+import React from 'react';
+
+type Props = {
+  open: boolean;
+};
+
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff'
+  }
+}));
+
+const LoadingBackdrop = (props: Props) => {
+  const classes = useStyles();
+
+  return (
+    <Backdrop className={classes.backdrop} open={props.open}>
+      <CircularProgress color="inherit" />
+    </Backdrop>
+  );
+};
+
+export default LoadingBackdrop;
