eb15a25127bef1348e369e942f891344bbea8eba
tranquochung6810 <hung.tran@bedigital.vn>
2020-12-12 21:56:53 +0700

    promo code

--- src/components/Modules/PromoCodes/PromoCodeItem.tsx
+++ src/components/Modules/PromoCodes/PromoCodeItem.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 
 type Props = {
-  type: string;
+  rewardType: string;
   couponName?: string;
   couponCode: string;
-  couponAmount?: string;
   couponDescription?: string;
   discount?: string;
+  couponDateFrom?: string;
 };
 
 export default function PromoCodeItem(props: Props) {
   const imgUrl =
     'https://assets.thuocsi.vn/assets/defaults/qua_tang-d6b9623d40a9924ac56d42815f5ed983f93ef06a88ec91c36261bb3bbb222553.jpg';
-  const { type } = props;
+  const { rewardType } = props;
 
-  const leftPromoCode = (type) => {
-    switch (type) {
-      case 'no_code_needed':
+  const leftPromoCode = (rewardType) => {
+    switch (rewardType) {
+      case 'product':
         return (
           <div className="mb-1 benefit">
             <div className="discount">
@@ -25,11 +25,11 @@ export default function PromoCodeItem(props: Props) {
             <div className="suffix">Quà Tặng</div>
           </div>
         );
-      case 'code_needed':
+      case 'discount':
         return (
           <div className="mb-1 benefit">
             <div className="discount">
-              {props.discount}
+              {`discount ${props.discount}%`}
               {/* <span className="unit">đ</span> */}
             </div>
             <div className="suffix">Giảm Giá</div>
@@ -40,36 +40,17 @@ export default function PromoCodeItem(props: Props) {
     }
   };
 
-  const titlePromoCode = (type) => {
-    switch (type) {
-      case 'no_code_needed':
-        return (
-          <div className="mb-2">
-            <div className="coupon__name">{props.couponName}</div>
-            <span>{props.couponDescription}</span>
-          </div>
-        );
-      case 'code_needed':
-        return (
-          <div className="mb-2">
-            <div className="coupon__amount">{props.couponName}</div>
-            <span>{props.couponDescription}</span>
-          </div>
-        );
-      default:
-        return;
-    }
-  };
-
   return (
-    <div className={`row no-gutters coupon coupon--${props.type}`}>
+    <div className={`row no-gutters coupon coupon--${props.rewardType}`}>
       <div className="col-4 deal-info pr-3 d-flex flex-column justify-content-between align-items-center">
-        {leftPromoCode(type)}
-        <div className="text-muted">Còn 05 ngày 11:13:54</div>
+        {leftPromoCode(rewardType)}
+        {props.couponDateFrom !== 'false' && (
+          <div className="text-muted">{`HSD: ${props.couponDateFrom}`}</div>
+        )}
       </div>
       <div className="col-8 d-flex flex-column justify-content-between text-center">
         <div className="mb-2">
-          <div className="coupon__amount">{props.couponName}</div>
+          <div className="coupon__amount text-max-2">{props.couponName}</div>
           <span>{props.couponDescription}</span>
         </div>
         {props.couponCode !== 'false' && (
