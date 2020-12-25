83710b3654973fdd6d6224b16ce36ec8b5ceb37f
Bùi Trung Bảo <buitrbao222@gmail.com>
2020-12-25 10:32:38 +0700

    Fix product card sale off translation

--- src/components/Modules/ProductCard/DiscountRibbon.tsx
+++ src/components/Modules/ProductCard/DiscountRibbon.tsx
@@ -1,12 +1,17 @@
+import { useTranslation } from 'i18n';
 import React from 'react';
 
 type Props = {
   discountPercent: number;
 };
 
-export const DiscountRibbon = (props: Props): JSX.Element => (
-  <div className="ribbon price_down">
-    <span className="ribbon-percent">{props.discountPercent}%</span>
-    <span className="ribbon-status">Giảm</span>
-  </div>
-);
+export const DiscountRibbon = (props: Props) => {
+  const { t } = useTranslation('productCard');
+
+  return (
+    <div className="ribbon price_down">
+      <span className="ribbon-percent">{props.discountPercent}%</span>
+      <span className="ribbon-status">{t('productCard:off')}</span>
+    </div>
+  );
+};
