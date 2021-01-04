2a1193a7c1338a16cd5c84ac42f9a500af6763d0
TrangJuny <trang.thai@bigin.vn>
2021-01-04 12:07:36 +0700

    update tawk.to

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -105,16 +105,16 @@ class MyDocument extends Document {
               __html: `
               document.addEventListener("DOMContentLoaded", function(event) {
                 setTimeout(function() {
-                  var Tawk_API = Tawk_API || {},
-                  Tawk_LoadStart = new Date();
-                  (function() {
-                  var s1 = document.createElement("script"),
-                  s0 = document.getElementsByTagName("script")[0];
-                  s1.async = true;
-                  s1.src = 'https://embed.tawk.to/5f97dbddaca01a16883609eb/default';
-                  s1.charset = 'UTF-8';
-                  s1.setAttribute('crossorigin', '*');
-                  s0.parentNode.insertBefore(s1, s0);
+                  var Tawk_API=Tawk_API||{},
+                   Tawk_LoadStart=new Date();
+                  (function(){
+                  var s1=document.createElement("script"),
+                  s0=document.getElementsByTagName("script")[0];
+                  s1.async=true;
+                  s1.src='https://embed.tawk.to/5fedb291df060f156a92a752/1eqs832ov';
+                  s1.charset='UTF-8';
+                  s1.setAttribute('crossorigin','*');
+                  s0.parentNode.insertBefore(s1,s0);
                   })();
                 }, 10000);
               });
