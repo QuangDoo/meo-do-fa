f7863f03ae577cd56acbc8a7be9fa0b6830ac35d
Bùi Trung Bảo <buitrbao222@gmail.com>
2021-01-08 20:44:37 +0700

    Remove Roboto font and slick css cdn (because we use react-slick css)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -53,24 +53,6 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
 
-          {/* Roboto Font */}
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
-            rel="stylesheet"
-          />
-
-          <link
-            rel="stylesheet"
-            type="text/css"
-            charSet="UTF-8"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-          />
-
           <style type="text/css">{globalStyles}</style>
         </Head>
         <body>
