bb602d5feff85415ce9833c0cb2675265f2db0a6
Bùi Trung Bảo <buitrbao222@gmail.com>
2021-01-10 13:15:02 +0700

    Loading backdrop fixed without scrollbar

--- src/components/Layout/LoadingBackdrop.tsx
+++ src/components/Layout/LoadingBackdrop.tsx
@@ -1,14 +1,16 @@
-import { Backdrop, CircularProgress, makeStyles } from '@material-ui/core';
+import { Backdrop, CircularProgress, Fade, makeStyles, Modal } from '@material-ui/core';
 import React from 'react';
 
 type Props = {
   open: boolean;
 };
 
-const useStyles = makeStyles((theme) => ({
-  backdrop: {
-    zIndex: theme.zIndex.drawer + 1,
-    color: '#fff'
+const useStyles = makeStyles(() => ({
+  modal: {
+    color: '#fff',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 }));
 
@@ -16,13 +18,17 @@ const LoadingBackdrop = (props: Props) => {
   const classes = useStyles();
 
   return (
-    <Backdrop
-      classes={{
-        root: classes.backdrop
-      }}
-      open={props.open}>
-      <CircularProgress color="inherit" />
-    </Backdrop>
+    <Modal
+      open={props.open}
+      className={classes.modal}
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 250
+      }}>
+      <Fade in={props.open}>
+        <CircularProgress color="inherit" />
+      </Fade>
+    </Modal>
   );
 };
 
