278a8c9373ef7f10e642ea5f3e1453bf69656896
quochungtran2000 <tranquochung6810@gmail.com>
2021-01-15 10:29:20 +0700

    Hotjar Tracking Code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -83,6 +83,21 @@ class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
+                (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:2197700,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+              `
+            }}
+          />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
               document.addEventListener("DOMContentLoaded", function(event) {
                 setTimeout(function() {
                   var Tawk_API=Tawk_API||{},
