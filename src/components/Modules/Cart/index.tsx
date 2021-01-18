cc562497ef80e4c697b9e59fb1a30d39137524cc
Bùi Trung Bảo <buitrbao222@gmail.com>
2021-01-18 19:34:23 +0700

    Add product name in error message when product changed price

--- src/components/Modules/Cart/index.tsx
+++ src/components/Modules/Cart/index.tsx
@@ -23,8 +23,21 @@ export default function CartPage() {
     onCompleted: () => {
       router.push('/checkout');
     },
-    onError: (error) => {
-      toast.error(t(`errors:code_${error.graphQLErrors?.[0]?.extensions?.code}`));
+    onError: (err) => {
+      const errorCode = err.graphQLErrors?.[0]?.extensions?.code;
+
+      if (errorCode === 121) {
+        toast.error(
+          t(`errors:code_${errorCode}`, {
+            name: err.graphQLErrors[0].message.replace(
+              'Sales price changed. Please remove product on cart. Product: ',
+              ''
+            )
+          })
+        );
+      } else {
+        toast.error(t(`errors:code_${errorCode}`));
+      }
     }
   });
 
