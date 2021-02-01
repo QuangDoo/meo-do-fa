a484dc0c001a62fe98f25c2d96811dbc8bc8d689
Bùi Trung Bảo <buitrbao222@gmail.com>
2021-02-01 19:53:30 +0700

    Fix ssr checkout page

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -2,11 +2,22 @@ import React from 'react';
 import Head from 'src/components/Layout/Head';
 import CheckoutPage from 'src/components/Modules/Checkout';
 import MainLayout, { mainLayoutNamespacesRequired } from 'src/components/Modules/MainLayout';
+import { GET_COUNSEL } from 'src/graphql/order/getCounsel';
+import asyncQuery from 'src/utils/asyncQuery';
 import withToken from 'src/utils/withToken';
 
-Checkout.getInitialProps = async () => ({
-  namespacesRequired: [...mainLayoutNamespacesRequired, 'checkout', 'myAccount']
-});
+Checkout.getInitialProps = async (ctx) => {
+  await asyncQuery({
+    ctx,
+    query: GET_COUNSEL,
+    fetchPolicy: 'network-only',
+    notifyOnNetworkStatusChange: true
+  });
+
+  return {
+    namespacesRequired: [...mainLayoutNamespacesRequired, 'checkout', 'myAccount']
+  };
+};
 
 function Checkout() {
   return (
