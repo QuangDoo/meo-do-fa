6984732fb2e7fe5b782344481558be75330687e6
quochungtran2000 <tranquochung6810@gmail.com>
2021-02-25 17:05:38 +0700

    fix regex tãxcode

--- src/assets/regex/taxCode.ts
+++ src/assets/regex/taxCode.ts
@@ -1,4 +1,4 @@
-export const taxCodeRegex = /(^[0-9]{10}$)|(^[0-9]{10}-[0-9]{3}$)/g;
+export const taxCodeRegex = /(^[0-9]{10}$)|(^[0-9]{13}$)|(^[0-9]{10}-[0-9]{3}$)/;
 
 // 10 numbers tax code: 0123456789
-// 13 numbers tax code: 0123456789-123
+// 13 numbers tax code: 0123456789-123 | 0123456789123
