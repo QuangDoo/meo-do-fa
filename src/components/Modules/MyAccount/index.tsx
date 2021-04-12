cd946292a5460a38e8a51231503ab44cf00c9b3f
Bùi Trung Bảo <buitrbao222@gmail.com>
2021-04-12 20:57:25 +0700

    Fix user undefined .vat

--- src/components/Modules/MyAccount/index.tsx
+++ src/components/Modules/MyAccount/index.tsx
@@ -190,7 +190,7 @@ export default function MyAccountPage() {
       });
   };
 
-  const vat = user?.vat?.replace('-', ' - ');
+  const vat = user?.vat?.replace('-', ' - ') || '';
 
   const onError = (error) => {
     toast.error(error[Object.keys(error)[0]].message);
