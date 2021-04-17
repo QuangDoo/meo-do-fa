ea95c62c3da9f54ff92b4fe742aa247fa01c33b2
Tuan Nguyen <tuan.nguyen@bebuy.vn>
2021-04-17 23:16:44 +0700

    fix: FILES_GATEWAY_EXT

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
 
 export const FILES_GATEWAY = `https://${
-  publicRuntimeConfig.NEXT_PUBLIC_FILES_GATEWAY || process.env.NEXT_PUBLIC_FILES_GATEWAY
+  publicRuntimeConfig.FILES_GATEWAY_EXT || process.env.NEXT_PUBLIC_FILES_GATEWAY
 }`;
 
 export const MAX_CERTIFICATES = 6;
