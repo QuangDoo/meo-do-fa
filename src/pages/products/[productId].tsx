c2df2e9ec1460eab2f3096450acd53bb0c460a32
Phú Huỳnh Trần <phu.ht@medofa.com>
2021-05-11 22:30:25 +0700

    share link version 02

--- src/pages/products/[productId].tsx
+++ src/pages/products/[productId].tsx
@@ -101,22 +101,12 @@ function ProductDetail() {
     <MainLayout>
       <Head>
         <title>Medofa - {product?.name}</title>
+        <meta property="og:title" content={product?.name} />
         <meta
-          content="Medofa là giải pháp công nghệ ngành dược kết nối nhà máy, nhà phân phối, nhà thuốc, dược sĩ nhằm hợp tác tạo mối liên kết trực tiếp và gia tăng doanh thu."
-          name="description"
-        />
-        <meta content="index, follow" name="robots" />
-        <meta content="website" property="og:type" />
-        <meta content="vi_VN" property="og:locale" />
-        <meta content={product?.name} property="og:title" />
-        <meta
-          content="Medofa là giải pháp công nghệ ngành dược kết nối nhà máy, nhà phân phối, nhà thuốc, dược sĩ nhằm hợp tác tạo mối liên kết trực tiếp và gia tăng doanh thu."
           property="og:description"
+          content="Medofa là giải pháp công nghệ ngành dược kết nối nhà máy, nhà phân phối, nhà thuốc, dược sĩ nhằm hợp tác tạo mối liên kết trực tiếp và gia tăng doanh thu."
         />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-        />
+        <meta property="og:url" content="https://medofa.com/" />
         <meta property="og:image" content={product?.image_128} />
       </Head>
 
