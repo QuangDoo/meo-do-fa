268b821c69dc7add193dcd893b38ef6526235c1c
Phú Huỳnh Trần <phu.ht@medofa.com>
2021-05-11 15:48:27 +0700

    Add name and image when share link

--- src/pages/career/index.tsx
+++ src/pages/career/index.tsx
@@ -11,6 +11,26 @@ const CareerPage = () => {
     <MainLayout>
       <Head>
         <title>Medofa</title>
+        <meta
+          content="Medofa là giải pháp công nghệ ngành dược kết nối nhà máy, nhà phân phối, nhà thuốc, dược sĩ nhằm hợp tác tạo mối liên kết trực tiếp và gia tăng doanh thu."
+          name="description"
+        />
+        <meta content="index, follow" name="robots" />
+        <meta content="website" property="og:type" />
+        <meta content="vi_VN" property="og:locale" />
+        <meta content="Career" property="og:title" />
+        <meta
+          content="Medofa là giải pháp công nghệ ngành dược kết nối nhà máy, nhà phân phối, nhà thuốc, dược sĩ nhằm hợp tác tạo mối liên kết trực tiếp và gia tăng doanh thu."
+          property="og:description"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta
+          property="og:image"
+          content="https://medofa.com/icons/favicon-32x32-medofa-manifest-20129.png"
+        />
       </Head>
 
       <Career />
--- src/pages/products/[productId].tsx
+++ src/pages/products/[productId].tsx
@@ -101,6 +101,23 @@ function ProductDetail() {
     <MainLayout>
       <Head>
         <title>Medofa - {product?.name}</title>
+        <meta
+          content="Medofa là giải pháp công nghệ ngành dược kết nối nhà máy, nhà phân phối, nhà thuốc, dược sĩ nhằm hợp tác tạo mối liên kết trực tiếp và gia tăng doanh thu."
+          name="description"
+        />
+        <meta content="index, follow" name="robots" />
+        <meta content="website" property="og:type" />
+        <meta content="vi_VN" property="og:locale" />
+        <meta content={product?.name} property="og:title" />
+        <meta
+          content="Medofa là giải pháp công nghệ ngành dược kết nối nhà máy, nhà phân phối, nhà thuốc, dược sĩ nhằm hợp tác tạo mối liên kết trực tiếp và gia tăng doanh thu."
+          property="og:description"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta property="og:image" content="{product?.image_128}" />
       </Head>
 
       {/* <div className="product container py-2">
