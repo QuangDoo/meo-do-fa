96369296f12c3ac20c64ea68d35fdd75dc158263
Quoc Hung <quochung@Hungs-Mac.local>
2021-05-11 12:05:52 +0700

    facebook chat

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+'use strict';
 import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/core/styles';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
@@ -113,6 +114,29 @@ class MyDocument extends Document {
                   })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
                 }, 10000);
               });
+              `
+            }}
+          />
+
+          <div id="fb-root"></div>
+          <div className="fb-customerchat" attribution="page_inbox" page_id="105284771164047"></div>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.fbAsyncInit = function() {
+                FB.init({
+                  xfbml            : true,
+                  version          : 'v10.0'
+                });
+              };
+      
+              (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+                fjs.parentNode.insertBefore(js, fjs);
+              }(document, 'script', 'facebook-jssdk'));
               `
             }}
           />
