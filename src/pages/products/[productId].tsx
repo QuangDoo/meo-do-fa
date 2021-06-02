30af4b967bffe06bb3ecbe184bf09ad6834417b0
Quang <quang.do@bebuy.vn>
2021-06-02 16:37:29 +0700

    remove scroll modal image

--- src/pages/products/[productId].tsx
+++ src/pages/products/[productId].tsx
@@ -95,7 +95,6 @@ function ProductDetail() {
   const [subImages, setSubImages] = useState<string[]>([]);
 
   const settings = {
-    dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
