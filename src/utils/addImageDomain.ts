bd52e306176ae68da64062a60ba849c0abeb660d
chau.ntm <chau.ntm@medofa.com>
2021-11-15 09:24:43 +0700

    fix: change check condidtion from hostname to gateway url because of fixing typeof window is undefined

--- src/utils/addImageDomain.ts
+++ src/utils/addImageDomain.ts
@@ -1,13 +1,12 @@
 export const addImageDomainToContent = (content: string) => {
+  const uri = process.env.GRAPHQL_GATEWAY;
   content = content?.replace(/<img/g, `<img style="width:100%;height: auto; object-fit: contain"`);
-  if (typeof window !== 'undefined' && content) {
-    const { hostname } = window.location;
-    if (content?.includes('src="/web/image')) {
-      const imgDomain =
-        hostname === 'medofa.com' ? 'https://erp.medofa.com' : 'https://erp.dev.medofa.com';
-      content.replace(/src="/g, `src="${imgDomain}/`);
+  if (content?.includes('src="/web/image')) {
+    if (uri?.includes('graphql.medofa.com')) {
+      content = content?.replace(/src="/g, `src="https://erp.medofa.com`);
+    } else {
+      content = content?.replace(/src="/g, `src="https://erp.dev.medofa.com`);
     }
   }
-
   return content;
 };
