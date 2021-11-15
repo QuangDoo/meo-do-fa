fbe5f503481974eae516709d4b59fce1b990de9c
chau.ntm <chau.ntm@medofa.com>
2021-11-15 09:10:28 +0700

    fix: fix replace url image in AddImageDomain function

--- src/utils/addImageDomain.ts
+++ src/utils/addImageDomain.ts
@@ -1,11 +1,12 @@
 export const addImageDomainToContent = (content: string) => {
+  content = content?.replace(/<img/g, `<img style="width:100%;height: auto; object-fit: contain"`);
   if (typeof window !== 'undefined' && content) {
     const { hostname } = window.location;
-
-    const imgDomain =
-      hostname === 'medofa.com' ? 'https://erp.medofa.com' : 'https://erp.dev.medofa.com';
-
-    return content.replace(/src="/g, `src="${imgDomain}/`);
+    if (content?.includes('src="/web/image')) {
+      const imgDomain =
+        hostname === 'medofa.com' ? 'https://erp.medofa.com' : 'https://erp.dev.medofa.com';
+      content.replace(/src="/g, `src="${imgDomain}/`);
+    }
   }
 
   return content;
